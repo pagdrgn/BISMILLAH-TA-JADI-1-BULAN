6469269bc0a1e92f532a0a001b5637fca84c10dc
pagdrgn <andinityasasih28@gmail.com>
2022-07-03 11:36:35 +0700

    second layouting for login, register, and main

--- app/src/main/java/com/example/ecommercebismillahtalancar1bulanjadi/LoginActivity.kt
+++ app/src/main/java/com/example/ecommercebismillahtalancar1bulanjadi/LoginActivity.kt
@@ -9,6 +9,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
         intentButtonDaftar()
     }
     private fun intentButtonDaftar() {
--- app/src/main/java/com/example/ecommercebismillahtalancar1bulanjadi/RegisterActivity.kt
+++ app/src/main/java/com/example/ecommercebismillahtalancar1bulanjadi/RegisterActivity.kt
@@ -9,6 +9,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
         intentButtonMasuk()
     }
     private fun intentButtonMasuk () {
